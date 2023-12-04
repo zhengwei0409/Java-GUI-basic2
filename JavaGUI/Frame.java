f9ff6195af28c081cb112a2eb50775c7ebb3d33c
ZhengWei <zhengwei0409@gmail.com>
2023-12-05 00:12:30 +0800

    added lambda expression

--- JavaGUI/Frame.java
+++ JavaGUI/Frame.java
@@ -1,19 +1,24 @@
 package JavaGUI;
 
-import java.awt.Button;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class Frame extends JFrame implements ActionListener {
+public class Frame extends JFrame {
 
-    Button button; // make it become global variable
+    JButton button; // make it become global variable
     Frame() {
 
-        button = new Button();
-        button.setBounds(250, 250, 100, 25);
-        button.addActionListener(this);
+        button = new JButton();
+        button.setBounds(250, 250, 200, 55);
+        button.setText("Play Now");
+        button.setFocusable(false);
+        button.addActionListener(e -> System.out.println("Don touchh me !"));
+        button.setFont(new Font("Helvetica Neue", Font.BOLD, 10));
+        button.setForeground(Color.green);
+        button.setBackground(Color.blue);
 
         this.setSize(500, 500);
         this.setLayout(null);
@@ -22,10 +27,4 @@ public class Frame extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==button) {
-            System.out.println("Dont touch me!!!");
-        }
-    }
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Java-GUI-basic2
-Added Label
+Added Label , Button 
 
 There is some technical issue in the previous code HAHAHAHHA , so i made a new repository
